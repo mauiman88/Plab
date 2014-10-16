6baea82126e50b09c32437f8c231789b44e19e2b
csabaujvari <csaba.ujvari.sys@gmail.com>
2014-10-16 17:54:22 +0200

    enable evolution plugin

--- app/controllers/OrderListController.java
+++ app/controllers/OrderListController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.User;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.db.ebean.Transactional;
@@ -8,12 +9,15 @@ import play.db.ebean.Transactional;
 /**
  * Created by gymate on 2014.10.14..
  */
-
-
 public class OrderListController extends Controller {
 
     @Transactional
     public static Result list() {
+        User user = new User();
+        user.email = "test@example.com";
+        user.password = User.hashPassword("test");
+        user.name = "Test";
+        user.save();
         return ok(views.html.Order.orderList.render());
     }
 
